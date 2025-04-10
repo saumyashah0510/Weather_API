6b85219b92ba7937b555abf904871cd3985a1e8c
saumyashah0510 <202401472@daiict.ac.in>
2025-04-10 14:57:20 +0530

    first commit

---
+++ weather.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import requests
+import os
+
+load_dotenv()
+
+def get_current_weather(city = "Ahmedabad"):
+    
+    request_url = f'http://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial'
+    
+    weather_data = requests.get(request_url).json()
+    return weather_data
+
+if __name__ == "__main__":
+    print("\n*** Get Current Weather Conditions ***")
+    city = input("Please enter a city name : ")
+    weather_data = get_current_weather(city)
+    print()
+    print(weather_data)
